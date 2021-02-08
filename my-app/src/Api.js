fadca2fdd744cbd790a1dbdd20e202716e561a26
fionach <fionach38@gmail.com>
2021-02-08 16:17:35 +0800

    load when page bottom

--- my-app/src/Api.js
+++ my-app/src/Api.js
@@ -2,11 +2,35 @@ import React from 'react';
 class Api extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {datas: []};
+        this.state = {
+            datas: [],
+            count: 0
+        };
+        this.handleScroll = this.handleScroll.bind(this);
+        this.canLoad = true;
     }
 
     componentDidMount() {
         this.getItems();
+        window.addEventListener("scroll", this.handleScroll);
+    }
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.handleScroll);
+    }
+
+    handleScroll() {
+        const windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
+        const body = document.body;
+        const html = document.documentElement;
+        const docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+        const windowBottom = Math.round(windowHeight + window.pageYOffset);
+        if (windowBottom === docHeight && this.canLoad) {
+            this.canLoad = false;
+            this.getItems();
+            //console.log("bottom!");
+        } else {
+            //console.log("not at bottom!");
+        }
     }
 
     getItems = () => {
@@ -16,13 +40,20 @@ class Api extends React.Component {
         axios.get('https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot', {
             params: {
                 $top:30,
+                $skip:`${that.state.count}`,
                 $format: 'JSON'
             }
         })
         .then(function (response) {
             // handle success
-            console.log(response.data);
-            that.setState({datas:response.data});
+            //console.log(response.data);
+            //that.setState({datas:response.data});
+            that.setState({
+                datas:that.state.datas.concat(response.data),
+                count:that.state.count+30
+            });
+            that.canLoad = true;
+            console.log(that.state.count);
         })
         .catch(function (error) {
             // handle error
@@ -40,9 +71,7 @@ class Api extends React.Component {
         
     render() {
         return (
-            <div className="App">
-              <div className="container">{this.listItems()}</div>
-            </div>
+            <div className="container">{this.listItems()}</div>
         );
     }
   }
